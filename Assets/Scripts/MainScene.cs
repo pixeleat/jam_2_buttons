f901d9c662d4e1e9c5833b147e66ddb215cb1f44
ada0l <andreika.varfolomeev@yandex.ru>
2020-12-13 12:40:21 +1000

    MainScene class

--- Assets/Scripts/MainScene.cs
+++ Assets/Scripts/MainScene.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class MainScene : MonoBehaviour
 {
+    State state;
     // Start is called before the first frame update
     void Start()
     {
